ff678ab16b4607b7ffa138ba6c6790158c6b1a29
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2022-10-18 11:15:31 +0200

    Fix #85

--- test/ampycloud/test_scientific_stability.py
+++ test/ampycloud/test_scientific_stability.py
@@ -11,6 +11,7 @@ Module content: tests for the scientific stability of the package
 # Import from Python
 from pathlib import Path
 import pickle
+import pytest
 import pandas as pd
 
 # Import from ampycloud
@@ -19,8 +20,21 @@ from ampycloud import dynamic, reset_prms
 from ampycloud.plots import diagnostic
 
 
-def test_scientific_stability(mpls: str, do_sciplots: bool) -> None:
-    """ Test the scientific stability of ampycloud. A series of reference cases is being processed,
+def get_cases():
+    """ Utility function that extracts a list of scientific cases (i.e. ref data files) to assess.
+
+    Fixes #85 by feeding the result to pytest.mark.parametrize().
+
+    """
+
+    # Where is the data located, and how many cases do I have ?
+    ref_data_path = Path(__file__).parent / 'ref_data'
+    return sorted(ref_data_path.glob('*.pkl'))
+
+
+@pytest.mark.parametrize("ref_data_file", get_cases())
+def test_scientific_stability(mpls: str, do_sciplots: bool, ref_data_file: Path) -> None:
+    """ Test the scientific stability of ampycloud. A specific cases is being processed,
     and the resulting METAR are compared with the expected outcome.
 
     Args:
@@ -30,6 +44,7 @@ def test_scientific_stability(mpls: str, do_sciplots: bool) -> None:
         do_sciplots (bool): True to make plots. This is set automatically
             by a fixture that fetches the corresponding command line argument.
             See conftest.py for details.
+        ref_data_file (Path): path+filename to the pickled test data.
 
     Important:
         We assess the scientific stability using only the METAR messages. This implies that any
@@ -41,47 +56,40 @@ def test_scientific_stability(mpls: str, do_sciplots: bool) -> None:
 
     """
 
-    # Where is the data located, and how many cases do I have ?
-    ref_data_path = Path(__file__).parent / 'ref_data'
-    ref_data_files = ref_data_path.glob('*.pkl')
+    # Extract the reference data
+    with open(ref_data_file, 'rb') as f:
+        data = pickle.load(f)
 
-    # Start processing every file
-    for ref_data_file in ref_data_files:
+    # Fix the dtype of the ceilo column
+    data.loc[:, 'ceilo'] = data.loc[:, 'ceilo'].astype(pd.StringDtype())
 
-        # Extract the reference data
-        with open(ref_data_file, 'rb') as f:
-            data = pickle.load(f)
+    # Get other useful info
+    geoloc = ref_data_file.stem.split('_')[0]
+    ref_dt = ref_data_file.stem.split('_')[1].replace('-', ' ')
+    ref_metar = ref_data_file.stem.split('_')[2].split('.')[0].replace('-', ' ')
 
-        # Fix the dtype of the ceilo column
-        data.loc[:, 'ceilo'] = data.loc[:, 'ceilo'].astype(pd.StringDtype())
+    # Do I need to set a specific MSA ?
+    if 'MSA' in ref_data_file.stem:
+        dynamic.AMPYCLOUD_PRMS['MSA'] = int(ref_data_file.stem.split('_')[3].split('.')[0][3:])
+    else:
+        # Make sure I use the default MSA
+        reset_prms()
 
-        # Get other useful info
-        geoloc = ref_data_file.stem.split('_')[0]
-        ref_dt = ref_data_file.stem.split('_')[1].replace('-', ' ')
-        ref_metar = ref_data_file.stem.split('_')[2].split('.')[0].replace('-', ' ')
+    # Run ampycloud
+    chunk = ampycloud.run(data, geoloc=geoloc, ref_dt=ref_dt)
 
-        # Do I need to set a specific MSA ?
-        if 'MSA' in ref_data_file.stem:
-            dynamic.AMPYCLOUD_PRMS['MSA'] = int(ref_data_file.stem.split('_')[3].split('.')[0][3:])
-        else:
-            # Make sure I use the default MSA
-            reset_prms()
+    # Should I make a plot ?
+    if do_sciplots:
+        # Do I need to set a specific plotting style ?
+        if mpls:
+            dynamic.AMPYCLOUD_PRMS['MPL_STYLE'] = mpls
 
-        # Run ampycloud
-        chunk = ampycloud.run(data, geoloc=geoloc, ref_dt=ref_dt)
+        # Create the plot
+        diagnostic(chunk, upto='layers', ref_metar=ref_metar, ref_metar_origin='Should be',
+                   show=False, save_stem=ref_data_file.stem, save_fmts=['png'])
 
-        # Should I make a plot ?
-        if do_sciplots:
-            # Do I need to set a specific plotting style ?
-            if mpls:
-                dynamic.AMPYCLOUD_PRMS['MPL_STYLE'] = mpls
+    # Was the computed METAR as expected ?
+    assert chunk.metar_msg() == ref_metar
 
-            # Create the plot
-            diagnostic(chunk, upto='layers', ref_metar=ref_metar, ref_metar_origin='Should be',
-                       show=False, save_stem=ref_data_file.stem, save_fmts=['png'])
-
-        # Was the computed METAR as expected ?
-        assert chunk.metar_msg() == ref_metar
-
-    # Reset the ampyclou parameters to avoid wreaking havoc with the other tests
+    # Reset the ampycloud parameters to avoid wreaking havoc with the other tests
     reset_prms()
