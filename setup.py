c69dd25b86a3c5445206dfa1f5c8da21d342a289
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2023-07-12 16:28:02 +0200

    Drop tests for python 3.8 and trigger them for 3.11

--- setup.py
+++ setup.py
@@ -40,14 +40,14 @@ setup(
     description="Characterization of cloud layers from ceilometer measurements",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    python_requires='>=3.8.0',
+    python_requires='>=3.9.0',
     install_requires=[
         "matplotlib >= 3.4.2",
         "numpy >= 1.20.3",
         "scikit-learn >= 1.2.0",
         "scipy >= 1.7.3",
         "statsmodels",
-        "pandas >= 1.3.1",
+        "pandas >= 1.5",
         "pyyaml",
         "ruamel.yaml"
     ],
