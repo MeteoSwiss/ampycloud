fb311cb48ebdd983aab23035c2fbcfc5cc31e6fe
Daniel Regenass <daniel.regenass@meteoswiss.ch>
2024-08-22 18:34:13 +0200

    AMAROC-835 request minimum amount of hits for base height calculation

--- src/ampycloud/data.py
+++ src/ampycloud/data.py
@@ -528,8 +528,12 @@ class CeiloChunk(AbstractChunk):
         for ind, cid in enumerate(cluster_ids):
             # Which hits are in this sli/gro/lay ?
             in_sligrolay = self.data[which[:-1]+'_id'] == cid
-            if self.prms()['CEILOS_FOR_BASE_HEIGHT_CALC'] != []:
-                in_sligrolay *= self.data['ceilo'].apply(lambda x: x in self.prms()['CEILOS_FOR_BASE_HEIGHT_CALC'])
+            if self.prms['CEILOS_FOR_BASE_HEIGHT_CALC'] != []:
+                in_sligrolay_filtered = in_sligrolay * self.data['ceilo'].apply(
+                    lambda x: x in self.prms['CEILOS_FOR_BASE_HEIGHT_CALC']
+                )
+                if in_sligrolay_filtered.sum() > 3: # require a minimum of 3 hits to calculate layer height
+                    in_sligrolay = in_sligrolay_filtered
             # Compute the base height
             pdf.iloc[ind, pdf.columns.get_loc('height_base')] = \
                 self._calculate_base_height_for_selection(in_sligrolay)
